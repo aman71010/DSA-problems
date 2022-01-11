3d6204c1a5c87362296318eefb4d612a9e2d4a19
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-01-11 23:30:19 +0530

    init

--- BinaryTree/postorder.cpp
+++ BinaryTree/postorder.cpp
@@ -41,11 +41,61 @@ class Solution
         }
         return res;
     }
+};
 
-    //using one stack
+class Solution 
+{
+    public:
+    // using two stack
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        stack<Node*> st1, st2;
+        Node* curr = root;
+        while(curr || !st1.empty()){
+            while(curr){
+                st1.push(curr);
+                st2.push(curr);
+                curr = curr->right;
+            }
+            Node* topNode = st1.top();
+            st1.pop();
+            curr = topNode->left;
+        }
+        while(!st2.empty()){
+            res.push_back(st2.top()->data);
+            st2.pop();
+        }
+        return res;
+    }
+};
 
-    // vector<int> postOrder(Node* root)
-    // {
-        
-    // }
+class Solution 
+{
+    public:
+    // using only one stack
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        stack<Node*> st;
+        Node* curr = root;
+        while(curr || !st.empty()){
+            while(curr){
+                if(curr->right)
+                    st.push(curr->right);
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            if(curr->right && !st.empty() &&curr->right == st.top()){
+                st.pop();
+                st.push(curr);
+                curr = curr->right;
+            }else{
+                res.push_back(curr->data);
+                curr = NULL;
+            }
+            
+        }
+        return res;
+    }
 };
