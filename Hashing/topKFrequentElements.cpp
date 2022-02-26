008029f1c42723b032af69e5ac3697ec04c113eb
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-02-26 17:53:45 +0530

    init

--- Hashing/topKFrequentElements.cpp
+++ Hashing/topKFrequentElements.cpp
@@ -1,39 +1,44 @@
 #include<bits\stdc++.h>
 using namespace std;
 
-// struct Compare(){
-//     bool operator()(pair<int, int> A, pair<int, int> B){
-//         if(A.second == B.second)
-//             return A.first>B.first;
-//         return A.second>B.second;
-//     }
-// }
-
-class Solution 
-{
+class Solution{
     public:
-    // vector<int> topK(vector<int>& nums, int k) {
-    //     unordered_map <int,int> mp;
-        
-    //     int n = nums.size();
-
-    //     for(int i=0; i<n; i++){
-    //         mp[nums[i]]++;
-    //     }
-
-    //     priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq(mp.begin(), mp.end());
-
-    //     vector<int> ans;
-        
-    //     for(int i=0; i<k; i++){
-            
-    //         ans.push_back(pq.top().first);
-    //     }
-
-    //     return ans;
-    // }
+    vector<int> topK(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i=0; i<n; i++)
+            mp[nums[i]]++;
+        priority_queue<pair<int, int>> pq;
+        for(auto x: mp)
+            pq.push({x.second, x.first});
+        vector<int> ans;
+        for(int i=0; i<k; i++){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
 };
 
-int main(){
-    return 0;
-}
+
+class Solution{
+    public:
+    vector<int> topK(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i=0; i<n; i++)
+            mp[nums[i]]++;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(auto x: mp){
+            pq.push({x.second, x.first});
+            if(pq.size()>k)
+                pq.pop();
+        }
+        vector<int> ans(k);
+        for(int i=k-1; i>=0; i--){
+            ans[i] = pq.top().second;
+            pq.pop();
+        }
+        return ans;
+    }
+};
