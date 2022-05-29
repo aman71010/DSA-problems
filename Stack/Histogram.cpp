9668f37642b61bfc4b0d5b5d50486792afd86bdf
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-05-29 16:49:45 +0530

    init

--- Stack/Histogram.cpp
+++ Stack/Histogram.cpp
@@ -36,3 +36,33 @@ class Solution
 int main(){
     return 0;
 }
+
+
+// T-> O(n) s-> O(n)
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int prev[n];
+        int next[n];
+        stack<int> st;
+        for(int i=0; i<n; i++){
+            while(!st.empty() && heights[st.top()]>=heights[i])
+                st.pop();
+            prev[i] = st.empty()? -1: st.top();
+            st.push(i);
+        }
+        while(!st.empty())
+            st.pop();
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && heights[st.top()]>=heights[i])
+                st.pop();
+            next[i] = st.empty()? n: st.top();
+            st.push(i);
+        }
+        int maxArea = INT_MIN;
+        for(int i=0; i<n; i++)
+            maxArea = max(maxArea, (next[i]-prev[i]-1)*heights[i]);
+        return maxArea;
+    }
+};
