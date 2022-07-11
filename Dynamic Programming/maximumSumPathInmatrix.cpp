1bd2c67ef62172937f236c1e810fb5fc8b761aac
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-07-11 13:03:17 +0530

    init

--- Dynamic Programming/maximumSumPathInmatrix.cpp
+++ Dynamic Programming/maximumSumPathInmatrix.cpp
@@ -8,39 +8,46 @@ public:
         int ans = 0;
         for(int i=0; i<n; i++){
             int sum = 0;
-            ans = max(ans, solve(mat, 0, i, sum, n));
+            ans = max(ans, solve(mat, 0, i, n));
         }
         return ans;
     }
 
-    int solve(vector<vector<int>> &mat, int r, int c, int sum, int n){
-        if(r<0 || r>=n || c>=n)
-            return sum;
-        sum += mat[r][c];
-        int left = solve(mat, r+1, c-1, sum, n);
-        int mid = solve(mat, r+1, c, sum, n);
-        int right = solve(mat, r+1, c+1, sum, n);
-        return max(left, max(mid, right));
+    bool isValid(int n, int r, int c){
+        return (r>=0 && r<n && c>=0 && c<n);
+    }
+
+    int solve(vector<vector<int>> &mat, int r, int c, int n){
+        if(r==n-1)
+            return mat[r][c];
+        int ma = -1;
+        if(isValid(n, r+1, c-1))
+            ma = max(ma, solve(mat, r+1, c-1, n));
+        if(isValid(n, r+1, c))
+            ma = max(ma, solve(mat, r+1, c, n));
+        if(isValid(n, r+1, c+1))
+            ma = max(ma, solve(mat, r+1, c+1, n));
+        return mat[r][c]+ma;
     }
 };
 
 class Solution{
 public:
     //dp 
-    bool isValid(int r, int c, int n){
-        return (c<0 || r>=n || c>=n);
+    bool isValid(int n, int r, int c){
+        return (r>=0 && r<n && c>=0 && c<n);
     }
 
     int maximumPath(int n, vector<vector<int>> mat){
         int ans = 0;
         int next[3] = {-1, 0, 1};
-        for(int i=n-1; i>=0; i--){
+        for(int i=n-2; i>=0; i--){
             for(int j=0; j<n; j++){
-                int m = mat[i][j];
-                for(int k=0; k<3; k++){
-                    if(!isValid(i+1, j+next[k], n))
-                        mat[i][j] = max(mat[i][j], m+mat[i+1][j+next[k]]);
-                }
+                int ma = -1;
+                for(int k=0; k<3; k++)
+                    if(isValid(n, i+1, j+next[k]))
+                        ma = max(ma, mat[i+1][j+next[k]]);
+                mat[i][j] += ma;
             }
         }
         for(int i=0; i<n; i++){
