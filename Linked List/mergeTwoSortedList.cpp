3dd2c59a9241b64ffe0ea45494c93eb3c1761ac1
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-07-13 12:16:40 +0530

    init

--- Linked List/mergeTwoSortedList.cpp
+++ Linked List/mergeTwoSortedList.cpp
@@ -12,17 +12,41 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l, ListNode* r) {
-        if(!l) return r;
-        if(!r) return l;
-        ListNode* curr = NULL;
-        if(l->val < r->val){
-            curr = l;
-            curr->next = mergeTwoLists(l->next, r);
+    ListNode* sortedMerge(ListNode* head1, ListNode* head2){  
+        if(!head1) return head2;
+        if(!head2) return head1;
+        if(head1->val <= head2->val){
+            head1->next = sortedMerge(head1->next, head2);
+            return head1;
         } else {
-            curr = r;
-            curr->next = mergeTwoLists(l, r->next);
+            head2->next = sortedMerge(head1, head2->next);
+            return head2;
         }
-        return curr;
     }
+};
+
+
+class Solution {
+public:
+    ListNode* sortedMerge(ListNode* head1, ListNode* head2){  
+    ListNode *dummy = new ListNode(-1);
+    ListNode *prev = dummy;
+    while(head1 && head2){
+        if(head1->val <= head2->val){
+            prev->next = head1;
+            head1 = head1->next;
+        } else {
+            prev->next = head2;
+            head2 = head2->next;
+        }
+        prev = prev->next;
+    }
+    if(head1)
+        prev->next = head1;
+    if(head2)
+        prev->next = head2;
+    head1 = dummy->next;
+    delete(dummy);
+    return head1;
+}  
 };
