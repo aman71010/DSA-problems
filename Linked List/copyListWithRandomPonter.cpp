8c944414ab148932e955b8748ba4c993f35c533c
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-07-16 14:49:20 +0530

    init

--- Linked List/copyListWithRandomPonter.cpp
+++ Linked List/copyListWithRandomPonter.cpp
@@ -22,72 +22,58 @@ public:
     Node* copyRandomList(Node* head) {
         if(!head)
             return NULL;
-        vector<Node*> v;
-        unordered_map<Node*, int> m;
+        unordered_map<Node*, Node*> m;
         Node *curr = head, *newHead = NULL, *prev = NULL;
-        int i=0;
         while(curr){
             Node *temp = new Node(curr->val);
+            m[curr] = temp;
             if(!newHead)
                 newHead = temp;
             else
                 prev->next = temp;
             prev = temp;
-            v.push_back(temp);
-            m[curr] = i++;
             curr = curr->next;
         }
         prev->next = NULL;
         Node *curr2 = newHead;
         curr = head;
         while(curr){
-            if(!curr->random){
-                curr2->random = NULL;
-            }
-            else{
-                curr2->random = v[m[curr->random]];
-            }
+            if(curr->random)
+                curr2->random = m[curr->random];
             curr = curr->next;
             curr2 = curr2->next;
         }
         return newHead;
     }
     //-------------------------------------------------------------------------------------------------
-    // T-> O(n) and S-> O(n)
+    // T-> O(n) and S-> O(1)
     Node* copyRandomList(Node* head) {
-        if(!head)
-            return NULL;
-        unordered_map<Node*, Node*> m;
-        Node *curr = head, *newHead = NULL, *prev = NULL;
+        if(!head) return NULL;
+        Node *curr = head;
         while(curr){
-            Node *temp = new Node(curr->val);
-            m[curr] = curr->next;
-            curr->next = temp;
-            temp->random = curr;
-            if(!newHead)
-                newHead = temp;
-            else
-                prev->next = temp;
-            prev = temp;
-            curr = curr->next;
-        }
-        prev->next = NULL;
-        curr = newHead;
-        while(curr){
-            if(!curr->random->random)
-                curr->random = NULL;
-            else
-                curr->random = curr->random->random->next;
-            curr = curr->next;
+            Node *temp = curr->next;
+            curr->next = new Node(curr->val);
+            curr->next->next = temp;
+            curr = temp;
         }
         curr = head;
         while(curr){
-            curr->next = m[curr];
-            curr = curr->next;
+            if(curr->random)
+                curr->next->random = curr->random->next;
+            curr = curr->next->next;
         }
-        return newHead;
+        curr = head;
+        Node* copy = head->next;
+        Node* res = head->next;
+        while(curr && copy){
+            curr->next = curr->next->next;
+            curr = curr->next;
+            if(curr)
+                copy->next = curr->next;
+            else
+                break;
+            copy = copy->next;
+        }
+        return res;
     }
-
-    
-
 };
