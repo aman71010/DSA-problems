6fb100905df293a54a59018a401e306093b69dba
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-07-21 21:48:18 +0530

    init

--- BinarySearchTree/sortedArrToBalancedBST.cpp
+++ BinarySearchTree/sortedArrToBalancedBST.cpp
@@ -1,30 +1,20 @@
 #include<bits\stdc++.h>
 using namespace std;
 
-
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
-};
-
-
 class Solution 
 {
-    Node* newNode(int k){
-        Node* temp = new Node;
-        temp->data = k;
-        temp->left = temp->right = NULL;
+    void funcUtil(vector<int> nums, vector<int> &res, int start, int end){
+        if(start<=end){
+            int mid = start + (end-start)/2;
+            res.push_back(nums[mid]);
+            funcUtil(nums, res, start, mid-1);
+            funcUtil(nums, res, mid+1, end);
+        }
     }
-
-    Node* constructBST(vector<int> &nums, int start, int end){
-        if(start>end)
-            return NULL;
-        int mid = (start+end)/2;
-        Node *root = newNode(mid);
-        root->left = constructBST(nums, start, mid-1);
-        root->right = constructBST(nums,  mid+1, end);
-        return root;
+    vector<int> sortedArrayToBST(vector<int>& nums) {
+        vector<int> res;
+        funcUtil(nums, res, 0, nums.size()-1);
+        return res;
     }
 
 };
