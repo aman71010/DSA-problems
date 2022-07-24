34b2769612dd615b739d8f57525971592bdfcf4f
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-07-24 18:56:49 +0530

    init

--- Stack/specialStack.cpp
+++ Stack/specialStack.cpp
@@ -1,6 +1,40 @@
 #include<bits\stdc++.h>
 using namespace std;
 
+class Solution{
+    int minEle;
+    stack<int> s;
+    stack<int> s2;
+    public:
+    
+        int getMin(){
+            if(s2.empty())
+                return -1;
+           return s2.top();
+        }
+       
+        int pop(){
+            if(s.empty())
+                return -1;
+            int t = s.top();
+            s.pop();
+            if(s2.top() == t)
+                s2.pop();
+            return t;
+        }
+       
+        void push(int a){
+            if(s.empty()){
+                s.push(a);
+                s2.push(a);
+            } else{
+                s.push(a);
+                if(a <= s2.top())
+                    s2.push(a);
+            }
+        }
+};
+
 
 //All operations have a time complexity of O(1) and Aux Space complexity of O(1)
 int d = 99999;
