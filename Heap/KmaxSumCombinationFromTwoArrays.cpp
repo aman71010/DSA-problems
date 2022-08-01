21fb012435c8cd6de9b20a754fc296935eb706b2
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-08-01 07:44:24 +0530

    init

--- Heap/KmaxSumCombinationFromTwoArrays.cpp
+++ Heap/KmaxSumCombinationFromTwoArrays.cpp
@@ -1,10 +1,24 @@
 #include<bits\stdc++.h>
 using namespace std;
 
-class Solution 
-{
+class Solution {
     public:
-    int function(int arr[], int n, int k){
-    	
+    //T->O(nlogk)  S->O(n)
+    vector<int> kMaxSumCombination(vector<int> &a, vector<int> &b, int n, int k){
+        vector<int> res;
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>> maxHeap;
+        for(int i=n-1; i>=0; i--)
+            maxHeap.push({a[i]+b[n-1], {i, n-1}});
+        while(!maxHeap.empty() && k--){
+            auto t = maxHeap.top();
+            maxHeap.pop();
+            res.push_back(t.first);
+            int i = t.second.first, j = t.second.second;
+            if(j!=0)
+                maxHeap.push({a[i]+b[j-1], {i,j-1}});
+        }
+        return res;
     }
 };
