e7bf175671f8b962ee0edcc6d2aa4997710cddbf
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-08-04 01:48:28 +0530

    init

--- Heap/MedianInAStream.cpp
+++ Heap/MedianInAStream.cpp
@@ -5,24 +5,48 @@ class Solution
 {
     public:
     
-    priority_queue<int, vector<int>> MaxHeap;
-    priority_queue<int, vector<int>, greater<int>> MinHeap;
+    priority_queue<int, vector<int>> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
     
     //Function to insert heap.
     void insertHeap(int &x)
     {
-        if(MinHeap.size()<MaxHeap.size())
-            MinHeap.push(x);
-        else
-            MaxHeap.push(x);
+        if(maxHeap.size() > minHeap.size()){
+            if(x > maxHeap.top())
+                minHeap.push(x);
+            else{
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+                maxHeap.push(x);
+            }
+        } else if(minHeap.size() > maxHeap.size()){
+            if(x < minHeap.top())
+                maxHeap.push(x);
+            else{
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+                minHeap.push(x);
+            }
+        } else {
+            if(maxHeap.empty() && minHeap.empty()){
+                maxHeap.push(x);
+                return;
+            }
+            if(x > minHeap.top())
+                minHeap.push(x);
+            else
+                maxHeap.push(x);
+        }
     }
     
     //Function to return Median.
     double getMedian()
     {
-        if(MaxHeap.size()==MinHeap.size())
-            return (double) (MaxHeap.top() + MinHeap.top())/2;
+        if(maxHeap.size()==minHeap.size())
+            return (double) (maxHeap.top() + minHeap.top())/2;
+        else if(maxHeap.size() > minHeap.size())
+            return maxHeap.top();
         else
-            return MaxHeap.top();
+            return minHeap.top();
     }
 };
