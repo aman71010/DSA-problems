703e5c4e4fc2cf17f3910acda78759cceb504c53
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-08-10 07:57:34 +0530

    init

--- array/mergeIntervals.cpp
+++ array/mergeIntervals.cpp
@@ -8,7 +8,6 @@ public:
         int n = intervals.size();
         sort(intervals.begin(), intervals.end());
         int j = 0;
-        intervals.push_back(intervals[0]);
         for(int i=1; i<n; i++){
             if(intervals[j][1]>=intervals[i][0])
                 intervals[j][1] = max(intervals[i][1], intervals[j][1]);
