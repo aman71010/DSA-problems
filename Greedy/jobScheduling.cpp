6543b592b69074055a598e3516d01f0830e815f0
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2022-08-27 11:22:52 +0530

    init

--- Greedy/jobScheduling.cpp
+++ Greedy/jobScheduling.cpp
@@ -15,7 +15,7 @@ class Solution
         return a.profit>b.profit;
     }
     //Function to find the maximum profit and the number of jobs done.
-    // T-> O(N*N) S-> O(N)
+    // T-> O(N*2) S-> O(N)
     vector<int> JobScheduling(Job arr[], int n) 
     { 
         sort(arr, arr+n, compare);
@@ -37,6 +37,43 @@ class Solution
                 count++;
             }
                 
+        }
+        return {count, profit};
+    } 
+};
+
+
+class Solution {
+    public:
+    
+    struct compare {
+        bool operator()(Job &a, Job &b){
+            return a.profit<b.profit;
+        }
+    };
+    // T-> O(N*logN) S-> O(N)
+    vector<int> JobScheduling(Job arr[], int n) { 
+        sort(arr, arr+n, [&](Job &a, Job&b){
+            return a.dead<b.dead;
+        });
+        priority_queue<Job, vector<Job>, compare> maxHeap;
+        vector<Job> results;
+        int slotsAvailable = 0;
+        for(int i=n-1; i>=0; i--){
+            if(i == 0)
+                slotsAvailable = arr[i].dead;
+            else
+                slotsAvailable = arr[i].dead-arr[i-1].dead;
+            maxHeap.push(arr[i]);
+            while(slotsAvailable>0 && maxHeap.empty()==false){
+                results.push_back(maxHeap.top());
+                maxHeap.pop();
+                slotsAvailable--;
+            }
+        }
+        int profit = 0, count = results.size();
+        for(int i=0; i<results.size(); i++){
+            profit += results[i].profit; 
         }
         return {count, profit};
     } 
